56035b2c03c6219425715f4ed327cb7e9d3b957f
Tarang <ptarang146@gmail.com>
2026-02-11 02:07:47 +0530

    User Profile Update.

---
+++ Online-Real-Estate-System-main/src/main/java/com/realestate/onlinerealestate/dto/UserUpdateRequest.java
@@ -0,0 +1,60 @@
+package com.realestate.onlinerealestate.dto;
+
+public class UserUpdateRequest {
+    private String fullName;
+    private String mobileNumber;
+    private String profilePicture;
+    private String bio;
+    private String city;
+    private String state;
+
+    public UserUpdateRequest() {}
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
